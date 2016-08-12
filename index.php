42028dcc9d713ba0f27f0080c268f16d8efa31f4
Larry Shih <larryycshih@gmail.com>
2016-08-13 00:30:11 +1000

    another test commit
    pulled to phpinfo to a independent file

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 <title>Page Title</title>
 </head>
 <body>
+<div id="header"></div>
+<div id="nav"></div>
 
 <h1>This is a Heading</h1>
 <p>This is a paragraph.</p>
 
 <br>
 
-<?php
-phpinfo();
-?>
+<div id="footer"></div>
 
 </body>
 </html>
---
+++ php_info.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>PHP Info</title>
+</head>
+
+<body>
+<?php phpInfo(); ?>
+</body>
+
+</html>
