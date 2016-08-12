0e6e1058fd64088a58619125bbcf3181d607bcfb
Larry Shih <larryycshih@gmail.com>
2016-08-12 23:58:11 +1000

    this is a test commit

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+<br>
+
 <?php
-phpinfo()
+phpinfo();
 ?>
+
+</body>
+</html>
