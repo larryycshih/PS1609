ccda0b7e66cf420acb05589b1b741c8809281862
Larry Shih <larryycshih@gmail.com>
2016-08-15 00:13:34 +1000

    added w3-css script
    added w3 css script and some testing on it.
    also added the wsu logo

--- index.php
+++ index.php
@@ -2,13 +2,31 @@
 <html>
 <head>
 <title>Page Title</title>
-</head>
-<body>
-<div id="header"></div>
-<div id="nav"></div>
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 
-<h1>This is a Heading</h1>
+</head>
+<body class="w3-container w3-padding-12">
+
+<nav class="w3-sidenav">
+<br>
+<h3>item 1</h3>
+<h3>item 1</h3>
+<h3>item 1</h3>
+<h3>item 1</h3>
+</nav>
+
+<div style="margin-left:25%">
+<div id="header" class="w3-container w3-red w3-padding-12">
+<img alt="wsu_logo" src="image/logo/wsu-logo.png">
+</div>
+
+
+<h1>This is a Heading&nbsp;&nbsp;&nbsp; </h1>
 <p>This is a paragraph.</p>
+
+<div class="w3-card">
+This is a card
+</div>
 
 <br>
 
