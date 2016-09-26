3a41642b8e0b0a0f2d9fabc4c0c821d2970f266f
Larry Shih <larryycshih@gmail.com>
2016-09-26 17:43:57 +1000

    manual merge

---
+++ Research Portal/Research Portal/Controllers/SchoolsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Research_Portal.Controllers
+{
+    public class SchoolsController : Controller
+    {
+        // GET: Schools
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Schools/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Schools/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schools/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Schools/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Schools/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Schools/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Schools/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Research Portal/Research Portal/Models/Record.cs
@@ -0,0 +1,17 @@
+
+namespace Research_Portal.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    public class Record
+    {
+        [Required]
+        public int ID { get; set; }
+        public int researchID { get; set; }
+        public string fileID { get; set; }
+    
+        public virtual Research Research { get; set; }
+    }
+}
